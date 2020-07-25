949eeb13dcd9147c1bb91ef1455e67afda33dbb8
Ekam <akashdeep0406@gmail.com>
2020-07-25 18:44:22 +0530

    Fixing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss'; 
+import './styles/styles.scss';
 
-const routes= (
-    <BrowserRouter>
-        
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<p></p>, document.getElementById('app'));
